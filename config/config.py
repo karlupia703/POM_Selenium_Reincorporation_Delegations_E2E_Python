2810407d67396998b9c572518f826a217580a4de
karlupiarajni <rajni.1156@zenmonk.tech>
2025-05-26 15:54:42 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,6 +3,6 @@ class Config:
     base_url = "https://reinstatements-delegations-reinstatement-responsg-iymj66chvq-uc.a.run.app/"
     language = "Español"  # Options: "Español", "en_US", "it_IT", "pt_BR"
     # Login Credentials
-    email = "zenmonk-developer-zenmonk@zenmonk.tech"
-    password = "a2fiEz5Sx2LzRiJ"
+    email = "username"
+    password = "password"
 
